5a0f511830b0cf742bb62fea2797aad457e73508
Imesh Vitharana <imeshpasinda@gmail.com>
2024-09-19 15:37:29 +0530

    Update ApiService.java

--- app/src/main/java/com/example/urbanmart/network/ApiService.java
+++ app/src/main/java/com/example/urbanmart/network/ApiService.java
@@ -11,7 +11,8 @@ import okhttp3.logging.HttpLoggingInterceptor;
 
 public class ApiService {
 
-    private static final String BASE_URL = "http://10.0.2.2:8082/api/";
+    private static final String BASE_URL_DEV = "http://10.0.2.2:8082/api/";
+    private static final String BASE_URL_PROD = "https://urbanmartapi-bqdwczd9gqcrdkc7.eastus-01.azurewebsites.net/api/";
     private static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
     private OkHttpClient client;
     private Gson gson;
@@ -33,7 +34,8 @@ public class ApiService {
 
         RequestBody body = RequestBody.create(json, JSON);
         Request request = new Request.Builder()
-                .url(BASE_URL + "users/login")
+                // Always use BASE_URL_DEV when committing to Git
+                .url(BASE_URL_DEV + "users/login")
                 .post(body)
                 .addHeader("Content-Type", "application/json")
                 .build();
